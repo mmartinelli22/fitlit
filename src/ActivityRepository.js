21b94aeb7a92c9ad893759ce1f4076baf861c73e
Cyanne Jones <jonescyanne@yahoo.com>
2022-06-03 18:21:17 -0600

    Add implementation code to pass getMilesWalked test

--- src/ActivityRepository.js
+++ src/ActivityRepository.js
@@ -1,7 +1,22 @@
+
+
 class ActivityRepository {
   constructor(data) {
     this.activityData = data
   }
+
+  getActivityDataForUser(idNum) {
+    const activityDataForUser = this.activityData.filter((user) => {
+        return user.userID === idNum;
+    });
+    return activityDataForUser;
+};
+
+  getMilesWalked(idNum, date, strideLength) {
+    const activityDataForUser = this.getActivityDataForUser(idNum);
+    const activityOnDay = activityDataForUser.find(day => day.date === date)
+   return parseFloat(((activityOnDay.numSteps * strideLength)/5280).toFixed(2));
+  };
 };
 
 export default ActivityRepository;
--- test/ActivityRepository-test.js
+++ test/ActivityRepository-test.js
@@ -1,12 +1,16 @@
 import { expect } from 'chai';
 import ActivityRepository from '../src/ActivityRepository';
+import UserRepository from '../src/ActivityRepository';
 import activityData from '../src/data/activity'
+import userData from '../src/data/users'
 
 describe('ActivityRepository', () => {
     let activityRepository;
+    let userRepository;
 
     beforeEach(() => {
       activityRepository = new ActivityRepository(activityData);
+      userRepository = new UserRepository(userData);
     })
 
     it('Should be a function', () => {
@@ -21,7 +25,70 @@ describe('ActivityRepository', () => {
         expect(activityRepository.activityData).to.equal(activityData);
     });
 
+    it('Should be able to get a specific user\'s activity data with their id', () => {
+      expect(activityRepository.getActivityDataForUser(1)).to.deep.equal(
+        [
+          {
+            userID: 1,
+            date: '2019/06/15',
+            numSteps: 3577,
+            minutesActive: 140,
+            flightsOfStairs: 16
+          },
+          {
+            userID: 1,
+            date: '2019/06/16',
+            numSteps: 6637,
+            minutesActive: 175,
+            flightsOfStairs: 36
+          },
+          {
+            userID: 1,
+            date: '2019/06/17',
+            numSteps: 14329,
+            minutesActive: 168,
+            flightsOfStairs: 18
+          },
+          {
+            userID: 1,
+            date: '2019/06/18',
+            numSteps: 4419,
+            minutesActive: 165,
+            flightsOfStairs: 33
+          },
+          {
+            userID: 1,
+            date: '2019/06/19',
+            numSteps: 8429,
+            minutesActive: 275,
+            flightsOfStairs: 2
+          },
+          {
+            userID: 1,
+            date: '2019/06/20',
+            numSteps: 14478,
+            minutesActive: 140,
+            flightsOfStairs: 12
+          },
+          {
+            userID: 1,
+            date: '2019/06/21',
+            numSteps: 6760,
+            minutesActive: 135,
+            flightsOfStairs: 6
+          },
+          {
+            userID: 1,
+            date: '2019/06/22',
+            numSteps: 10289,
+            minutesActive: 119,
+            flightsOfStairs: 6
+          }
+        ]
+      );
+    });
+
     it('Should be able to return miles a user walked for a specific date', () => {
-      expect(activityRepository.getMilesWalked(1, '2019/06/15')).to.equal(2.91);
+      expect(activityRepository.getMilesWalked(1, '2019/06/15', 4.3)).to.equal(2.91);
     });
 });
