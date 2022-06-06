2df82ea4353f318cd0468b3afd5d963d4fe06767
Michael Martinelli <turtelboy123@gmail.com>
2022-06-06 15:29:54 -0600

    Add html elements

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,15 +2,17 @@ function fetchApiData(url) {
     return fetch(url)
         .then(promise => promise.json())
 };
-function postApiData(url) {
+function postApiData(url, data) {
     fetch(url, {
         method: 'POST', headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
-    }).then(data => data.json()).then(data => console.log('It works', data))
+    }).then(data => data.json()).then(data => {
+        console.log('It works', data)
+        fetch(url)
+            .then(data => data.json())
+            .then(data => console.log('work pls', data))
+    })
         .catch(error => console.log(error));
-    fetch(url)
-        .then(data => data.json())
-        .then(data => console.log('work pls', data))
 }
 const data = { userID: 1, date: '2022/04/20', numOunces: 420 }
 export { fetchApiData, postApiData };
